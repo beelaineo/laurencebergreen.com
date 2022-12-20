f9e1268f93c9145eae13b29ef9185d03408502b7
Blaine <blaine.d.oneill@gmail.com>
2022-12-20 00:56:00 -0800

    fix(app): use env vars

--- app/sanityClient.ts
+++ app/sanityClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 const sanityClient = createClient({
-  projectId: 'oebpyzcq',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2022-12-16',
   useCdn: false,
 })
