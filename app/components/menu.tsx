287618e5709c4312022dd941fd60dd9ad3dcebdf
Blaine <blaine.d.oneill@gmail.com>
2023-02-02 14:26:09 -0800

    fix(app): style nav link hover states

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -18,7 +18,7 @@ export default function Menu() {
   const [settings, setSettings] = useState(null)
   const [books, setBooks] = useState(null)
   const { menuIsOpen, closeMenu } = useMenu()
-  const { asPath } = useRouter()
+  const { asPath, pathname } = useRouter()
 
   useEffect(() => {
     async function fetchData() {
@@ -48,28 +48,35 @@ export default function Menu() {
   }, [asPath])
 
   return (
+    <>
+    <style jsx global>{`
+      a.active {
+        color: #00000080;
+      }
+    `}</style>
     <div className={([styles.menu, menuIsOpen ? styles.open : '']).join(" ")}>
       <div className={styles.wrapper}>
         <nav className={styles.nav}>
           {settings?.nav_links.map((item: SanityKeyed<NavLinkType>, i: number) => (
-            <Link key={i} href={item.slug}>{item.title}</Link>
+            <Link key={i} href={item.slug} className={pathname == item.slug ? "active" : ""}>{item.title}</Link>
             )
           )}
         </nav>
         <div className={styles.books}>
           {books?.filter((item: SanityKeyed<BookType>) => item.category == 'book' ).map((item: SanityKeyed<BookType>, i: number) => (
-            <Link key={i} href={`/books/${item.slug}`}>{item.title}</Link>
+            <Link key={i} href={`/books/${item.slug}`} className={asPath == `/books/${item.slug}` ? "active" : ""}>{item.title}</Link>
             )
           )}
         </div>
         <div className={styles.books}>
           <h3>Young Adult Books</h3>
           {books?.filter((item: SanityKeyed<BookType>) => item.category == 'ya_book' ).map((item: SanityKeyed<BookType>, i: number) => (
-            <Link key={i} href={`/books/${item.slug}`}>{item.title}</Link>
+            <Link key={i} href={`/books/${item.slug}`} className={asPath == `/books/${item.slug}` ? "active" : ""}>{item.title}</Link>
             )
           )}
         </div>
       </div>
     </div>
+    </>
   )
 }
